d78039d0bc068318817ff8b47345889f400b0ff1
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-12-06 17:13:35 +0100

    Updated the test for editMyProfile

--- __tests__/test_cases/unit/editMyProfile.test.ts
+++ __tests__/test_cases/unit/editMyProfile.test.ts
@@ -7,13 +7,14 @@ import { SAMPLE_USER_PROFILE } from '../../mocks/user';
 const chance = Chance();
 const { ...user } = SAMPLE_USER_PROFILE;
 
+const templatePath = path.resolve(
+  __dirname,
+  '../../../mapping-templates/Mutation.editMyProfile.request.vtl',
+);
+const username = chance.guid();
+
 describe('Mutation.editMyProfile.request template', () => {
-  it("Should use 'input' fields in expression values", () => {
-    const templatePath = path.resolve(
-      __dirname,
-      '../../../mapping-templates/Mutation.editMyProfile.request.vtl',
-    );
-    const username = chance.guid();
+  it("Should use 'input' fields in expression values when all of them are provided", () => {
     const context = given.an_appsync_context(
       { username },
       { input: { ...user } },
@@ -39,6 +40,8 @@ describe('Mutation.editMyProfile.request template', () => {
         },
       },
       'update': {
+        'expression':
+          'set #name = :name, imageUrl = :imageUrl, backgroundImageUrl = :backgroundImageUrl, bio = :bio, #location = :location, website = :website, birthdate = :birthdate',
         'expressionValues': {
           ':name': { 'S': name },
           ':imageUrl': { 'S': imageUrl },
@@ -46,6 +49,57 @@ describe('Mutation.editMyProfile.request template', () => {
           ':bio': { 'S': bio },
           ':birthdate': { 'S': birthdate },
           ':location': { 'S': location },
+          ':website': { 'S': website },
+        },
+      },
+    });
+  });
+
+  it("Should use 'input' fields in expression values when some of them are null", () => {
+    const {
+      name,
+      backgroundImageUrl,
+      bio,
+      birthdate,
+      imageUrl,
+      location,
+      website,
+    } = user;
+    const context = given.an_appsync_context(
+      { username },
+      {
+        input: {
+          name,
+          backgroundImageUrl: null,
+          bio: null,
+          birthdate: null,
+          imageUrl,
+          location: null,
+          website,
+        },
+      },
+    );
+
+    const result = when.we_invoke_an_appsync_template(templatePath, context);
+
+    expect(result).toMatchObject({
+      'version': '2018-05-29',
+      'operation': 'UpdateItem',
+      'key': {
+        'id': {
+          'S': username,
+        },
+      },
+      'update': {
+        'expression':
+          'set #name = :name, imageUrl = :imageUrl, backgroundImageUrl = :backgroundImageUrl, bio = :bio, #location = :location, website = :website, birthdate = :birthdate',
+        'expressionValues': {
+          ':name': { 'S': name },
+          ':imageUrl': { 'S': imageUrl },
+          ':backgroundImageUrl': { 'NULL': true },
+          ':bio': { 'NULL': true },
+          ':birthdate': { 'NULL': true },
+          ':location': { 'NULL': true },
           ':website': { 'S': website },
         },
       },
